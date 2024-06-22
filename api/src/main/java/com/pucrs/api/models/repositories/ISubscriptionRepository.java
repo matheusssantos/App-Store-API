cb8888ee83196267bbcde20814326ee28291c56a
mauricioluz22 <161383052+mauricioluz22@users.noreply.github.com>
2024-06-22 18:10:52 +0000

    alterados parametros do metodo criaAssinatura em ISubscriptionRepository

--- api/src/main/java/com/pucrs/api/models/repositories/ISubscriptionRepository.java
+++ api/src/main/java/com/pucrs/api/models/repositories/ISubscriptionRepository.java
@@ -3,10 +3,12 @@ package com.pucrs.api.models.repositories;
 import java.util.List;
 
 import com.pucrs.api.models.Subscription;
+import com.pucrs.api.models.Client;
+import com.pucrs.api.models.App;
 
 public interface ISubscriptionRepository {
     List<Subscription> todos();
     Subscription acharPorId(Integer id);
     List<Subscription> assinaturasConformeTipo(String tipo);
-    Subscription criaAssinatura(Integer codCliente, Integer codApp);
+    Subscription criaAssinatura(Client cli, App app);
 }
